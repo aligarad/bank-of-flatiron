0ad7f7fa8e7d9fb7eb1c599fe357de9311b4dac7
kenhunja <garadcali190@gmail.com>
2023-01-17 00:48:18 +0300

    Bank of flatiron

---
+++ bank/src/App/App.js
@@ -0,0 +1,72 @@
+import { useEffect,useState } from "react";
+
+import './App.css';
+import Table from './components/Table.js';
+import Form from './components/form.js';
+import Searchbar from './components/searchBar.js';
+function App() {
+  const [transactions,setTransactions] = useState(null)
+  const [initialState,setInitialState] = useState(null)
+  
+  useEffect(() =>{
+  fetch("http://localhost:3000/transactions")
+  .then((response)=> response.json())
+  .then((res)=>{ 
+    sortByCategory(res)
+    setInitialState(res)
+  })
+  },[])
+let sortByCategory = (data) => {
+data.sort((a,b)=>{
+  let categoryA = a.category.toLowerCase()
+  let categoryB = b.category.toLowerCase()
+  
+  if(categoryA < categoryB ) {
+    return -1;
+}
+if (categoryA > categoryB ) {
+    return 1;
+}
+return 0;
+})
+  setTransactions(data)
+}
+  let handleSearch = (value) => {
+    
+    let income = transactions.filter((trans )=> {return trans.description.toLowerCase().includes(value.toLowerCase())})
+    if(income.length > 0){
+    console.log(income)
+    setTransactions(income)}
+    else{console.log("No transactions")}
+  }
+  
+  let handleBack = () =>{
+    setTransactions(initialState)
+  }
+  let [name, setName] = useState(false)
+  let [newName, setNewName] = useState(true)
+  let appendTransaction = ()=>{
+    setName(true)
+    setNewName(false)
+    console.log('true')
+  }
+  let removeTransaction = ()=>{
+    setNewName(true)
+    setName(false)
+    console.log('true')
+  }
+ 
+  let newClassName = newName ? "formVisibible" : "formHidden"
+  let className = name ? "formVisible" : "formHidden"
+  console.log(className)
+  return (
+    <div className="App">
+      <div className = {newClassName} id='mainContent'>
+      <Searchbar appendTransaction = {appendTransaction} handleBack = {handleBack} handleSearch = {handleSearch}/>
+      {transactions && <Table data = {transactions}/>}
+      </div>
+      <Form removeTransaction = {removeTransaction} className = {className} />
+    </div>
+  );
+}
+export default App;
