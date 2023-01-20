f0abcff6ddf944b3708c22074d4692297ecac24e
kenhunja <garadcali190@gmail.com>
2023-01-20 15:40:50 +0300

    completed assignment

--- src/App/App.js
+++ src/App/App.js
@@ -1,73 +1,40 @@
-import { useEffect,useState } from "react";
-
+import React, {useEffect, useState} from 'react';
+import Bank from '../components/Bank.js';
+import AddTransaction from '../components/AddTransaction.js';
 import './App.css';
-import Table from '../components/Table.js';
-import Form from '../components/form.js';
-import Searchbar from '../components/searchBar.js';
+
+const BANK_API = "https://my-json-server.typicode.com/aligarad/server/transactions"
+
+
+
 function App() {
-  const [transactions,setTransactions] = useState(null)
-  const [initialState,setInitialState] = useState(null)
-  
-  useEffect(() =>{
-  fetch("https://my-json-server.typicode.com/aligarad/server/transactions")
-  .then((response)=> response.json())
-  .then((res)=>{ 
-    sortByCategory(res)
-    setInitialState(res)
-  })
+  const [data, setData] = useState([])
+  console.log(data)
+  useEffect(()=>{
+    const intId = fetch(BANK_API)
+    .then((res)=>res.json())
+    .then((data)=> setData(data))
+    return function(){
+      clearInterval(intId)
+    }
   },[])
-let sortByCategory = (data) => {
-data.sort((a,b)=>{
-  let categoryA = a.category.toLowerCase()
-  let categoryB = b.category.toLowerCase()
   
-  if(categoryA < categoryB ) {
-    return -1;
-}
-if (categoryA > categoryB ) {
-    return 1;
-}
-return 0;
-})
-  setTransactions(data)
-}
-  let handleSearch = (value) => {
-    
-    let income = transactions.filter((trans )=> {return trans.description.toLowerCase().includes(value.toLowerCase())})
-    if(income.length > 0){
-    console.log(income)
-    setTransactions(income)}
-    else{console.log("No transactions")}
+    function addTransaction(newData) {
+      const updatedTransactions = [...data, newData]
+      setData(updatedTransactions);
+    }
+  
+  function deleteTodo(id) {
+    const updatedTransactions = data.filter(todo => todo.id !== id)
+    setData(updatedTransactions)
   }
   
-  let handleBack = () =>{
-    setTransactions(initialState)
-  }
-  let [name, setName] = useState(false)
-  let [newName, setNewName] = useState(true)
-  let appendTransaction = ()=>{
-    setName(true)
-    setNewName(false)
-    console.log('true')
-  }
-  let removeTransaction = ()=>{
-    setNewName(true)
-    setName(false)
-    console.log('true')
-  }
- 
-  let newClassName = newName ? "formVisibible" : "formHidden"
-  let className = name ? "formVisible" : "formHidden"
-  console.log(className)
   return (
-    <div className="App">
-      <div className="h2"><h2>Bank of FLATIRON</h2></div>
-      <div className = {newClassName} id='mainContent'>
-      <Searchbar appendTransaction = {appendTransaction} handleBack = {handleBack} handleSearch = {handleSearch}/>
-      {transactions && <Table data = {transactions}/>}
-      </div>
-      <Form removeTransaction = {removeTransaction} className = {className} />
-    </div>
+    <>
+    <h2 className='h2'> Bank Of Flatiron</h2>
+      <AddTransaction addTransaction={addTransaction}/>      
+      <Bank item={data} onDeleteTodo={deleteTodo}/>      
+    </>
   );
 }
 export default App;
---
+++ src/components/Transaction.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const BANK_API = "https://my-json-server.typicode.com/levy-web/flatiron-bank/transactions"
+
+
+function Transactions({id, date, amount, description, category, onDeleteTodo}){
+    function handleDelete() {
+        // persist changes on server
+        fetch(`${BANK_API}/${id}`, {
+            method: "DELETE"
+        })
+        // then use onDeleteTodo to remove todo from state
+        onDeleteTodo(id)
+        
+    }
+    
+    
+    return(
+        <>
+            <table className="tableTransactions">
+                <tbody>
+                    <tr>            
+                        <td>{date}</td>
+                        <td>{amount}</td>
+                        <td>{description}</td>
+                        <td>{category}</td>
+                        <td><button onClick={handleDelete} type="button">X</button></td>
+                    </tr>
+                </tbody>
+            </table>
+            
+        </>
+        
+    )
+}
+export default Transactions;
